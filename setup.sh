33b61896b20aca6757acff0fe6dc4b4e4f878a50
Oka Prinarjaya <okaprinarjaya@gmail.com>
2018-11-25 00:00:30 +0700

    Updating setup runner to add new migrations

--- setup.sh
+++ setup.sh
@@ -35,6 +35,9 @@ then
   $1 bin/cake.php migrations seed --seed SequentialWilayahSeed
   $1 bin/cake.php migrations seed --seed RelawanSeed
   $1 bin/cake.php migrations seed --seed UsiaSeed
+  $1 bin/cake.php migrations seed --seed PekerjaanSeed
+  $1 bin/cake.php migrations seed --seed PenghasilanSeed
+  $1 bin/cake.php migrations seed --seed RelawanProfileSeed
 
 else
   echo "Please set alternative PHP location"
